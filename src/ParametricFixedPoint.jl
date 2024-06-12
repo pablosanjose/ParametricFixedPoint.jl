b6e8dce9d347d63b4bc2243c38f1a4e7310aee95
Pablo San-Jose <lekand@gmail.com>
2024-06-12 15:53:55 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.50

---
+++ src/ParametricFixedPoint.jl
@@ -0,0 +1,5 @@
+module ParametricFixedPoint
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ParametricFixedPoint
+using Test
+
+@testset "ParametricFixedPoint.jl" begin
+    # Write your tests here.
+end
