251f3e4e709a0b0d54c98f91d952b351b54ae002
Pablo San-Jose <lekand@gmail.com>
2024-06-22 12:56:14 +0200

    make f! mutating

--- src/ParametricFixedPoint.jl
+++ src/ParametricFixedPoint.jl
@@ -4,38 +4,38 @@ export pfp, pfp!, pfp_store
 
 using SIAMFANLEquations: aasol
 
-# Solve fixed point x = f(x, p)
-pfp(f, x0, args...; kw...) = pfp!(f, copy(x0), args...; kw...)
+# Solve fixed point x = f(x, p). f must be given in mutating form, f!(x´, x, p[, pdata])
+# where pdata is an optional data object passed as a pdata keyword argument to pfp
+pfp(f!, x0, args...; kw...) = pfp!(f!, copy(x0), args...; kw...)
 
-function pfp!(f, x0, p0::Real = 1;
+function pfp!(f!, x0, p0::Real = 1;
     order = 2,
     f´ = pcurve, converge_error = false,
     store = pfp_store(x0, p0, order), kw...)
-    isextended, v0, vstore = store
+    x´, vstore = store
     len = length(v0)
-    function f!(vp´, vp)
+    function fext!(vp´, vp, pdata...)
         if isextended
             p = clamp(abs(last(vp)), 0.0, 1.0)
-            x = unvec!(x0, view(vp, 1:len-1))
+            x = copy!(x0, view(vp, 1:len-1))
         else
             p = 1.0
-            x = unvec!(x0, vp)
+            x = x0
         end
-        x´ = f(x, p)
+        f!(x´, x, p, pdata...)
         copyto!(vp´, x´)
         isextended && (vp´[len] = f´(p))
         return vp´
     end
-    sol = aasol(f!, v0, order, vstore)
+    sol = aasol(fext!, v0, order, vstore; kw...)
     return process_solution(sol, x0, isextended; converge_error)
 end
 
 function pfp_store(x0, p0, m)
     isextended = p0 ≈ 1.0
-    v0 = tovec(x0)
-    isextended && push!(v0, p0)
-    vstore = zeros(length(v0), max(3m+3, 2m+4))
-    return isextended, v0, vstore
+    x0´ = similar(x0)
+    vstore = zeros(length(v0)+isextended, max(3m+3, 2m+4))
+    return x0´, vstore
 end
 
 function process_solution(sol, x0, isextended; converge_error = false)
